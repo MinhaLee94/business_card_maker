0be10a69f35566d8d2653de58e16f243a21c44cc
MinhaLee94 <shjune1994@gmail.com>
2021-07-09 23:21:17 -0400

    Build Logout

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,33 +1,50 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import Header from "../header/header";
 import Footer from "../footer/footer";
 import styles from "./login.module.css";
 
-const Login = ({authService}) => {
-  const onLogin = event => {
-    authService.login(event.currentTarget.textContent);
+const Login = ({ authService }) => {
+  const history = useHistory();
+  const goToMaker = (userId) => {
+    history.push({
+      pathname: "/maker",
+      state: { id: userId },
+    });
   };
+
+  const onLogin = (event) => {
+    authService
+      .login(event.currentTarget.textContent)
+      .then((data) => goToMaker(data.user.uid));
+  };
+
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      user && goToMaker(user.id);
+    });
+  });
   return (
     <section className={styles.login}>
       <Header />
-        <section>
-          <h1>login</h1>
-          <ul className={styles.list}>
-            <li className={styles.item}>
-              <button className={styles.button} onClick={onLogin}>
-                Google
-              </button>
-            </li>
-            <li className={styles.item}>
-              <button className={styles.button} onClick={onLogin}>
-                Github
-              </button>
-            </li>
-          </ul>
-        </section>
+      <section>
+        <h1>login</h1>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <button className={styles.button} onClick={onLogin}>
+              Google
+            </button>
+          </li>
+          <li className={styles.item}>
+            <button className={styles.button} onClick={onLogin}>
+              Github
+            </button>
+          </li>
+        </ul>
+      </section>
       <Footer />
     </section>
-  )
-}
+  );
+};
 
 export default Login;
