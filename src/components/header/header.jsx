8b63bcdbf883fcc5588239a90c984519df83e61b
MinhaLee94 <shjune1994@gmail.com>
2021-12-29 23:52:24 -0500

    Build editor and preview

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,7 +4,7 @@ import styles from "./header.module.css";
 const Header = ({onLogout}) => (
   // when onLogout exists, show the button
   <header className={styles.header}>
-    {onLogout && (<button className={styles.logout}onClick={onLogout}>Logout</button>)} 
+    {onLogout && (<button className={styles.logout} onClick={onLogout}>Logout</button>)} 
     <img className={styles.logo}src="/images/logo.png" alt="logo" />
     <h1 className={styles.title}>Business Card Maker</h1>
   </header>
